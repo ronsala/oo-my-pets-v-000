232a101ea6f2f17fd51fcf96184162cb9e3856ec
Ron Sala <ron.sala@gmail.com>
2018-02-04 11:40:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,15 @@
 class Dog
+
   attr_reader :name
   attr_accessor :mood, :owner
 
-    def initialize(name)
-      @name = name
-      @mood = "nervous"
-    end
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 
-    def mood=(mood)
-      @mood = mood
-    end
+  def mood=(mood)
+    @mood = mood
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -42,4 +42,34 @@ class Owner
     fish = Fish.new(name)
     @pets[:fishes] << fish
   end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+
+  def walk_dogs
+    @pets[:dogs].each {|dog| dog.mood=("happy")}
+  end
+
+  def play_with_cats
+    @pets[:cats].each {|cat| cat.mood=("happy")}
+  end
+
+  def feed_fish
+    @pets[:fishes].each {|fish| fish.mood=("happy")}
+  end
+
+  def sell_pets
+    @pets[:dogs].each {|dog| dog.mood=("nervous")}
+    @pets[:cats].each {|cat| cat.mood=("nervous")}
+    @pets[:fishes].each {|fish| fish.mood=("nervous")}
+    # self.pets.each {|type, pets| pets = []}
+    @pets[:dogs].each {|dog| dog.mood=("nervous")}
+  end
 end
