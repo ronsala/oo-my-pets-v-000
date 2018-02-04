cc16904e422aa0512b811274f80bc9f41212a19d
Ron Sala <ron.sala@gmail.com>
2018-02-04 10:15:05 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,13 @@
 class Fish
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood, :owner
+
+    def initialize(name)
+      @name = name
+      @mood = "nervous"
+    end
+
+    def mood=(mood)
+      @mood = mood
+    end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,20 @@
+require "pry"
+
 class Owner
-  # code goes here
+  @@all = []
+  attr_accessor :pets
+
+  def initialize(name)
+    @name = name
+    @pets = {:fishes => [], :cats => [], :dogs => []}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+    # binding.pry
 end
