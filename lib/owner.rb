1d2b6f67b59baf4d76dd8e6a8d9780949a6f8235
Ron Sala <ron.sala@gmail.com>
2018-02-04 10:34:54 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,19 +2,39 @@ require "pry"
 
 class Owner
   @@all = []
+  @@count = 0
   attr_accessor :pets
+  attr_reader :species
 
   def initialize(name)
     @name = name
+    @species = "human"
     @pets = {:fishes => [], :cats => [], :dogs => []}
+    @@all << self
+    @@count += 1
   end
 
   def self.all
     @@all
   end
 
-  def save
-    @@all << self
+  def self.reset_all
+    @@count = 0
   end
-    # binding.pry
+
+  def self.count
+    @@count
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
 end
